cf6ff8aaf1db436f2c50459aff098c8df03b2d24
Alex Bance <abance@bancey.xyz>
2024-05-11 14:53:41 +0100

    feat: update certificate config

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -8,15 +8,16 @@ curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
 
 DOMAIN="${domain}"
+EMAIL="${email}"
 
-if [ ! -z "$DOMAIN" ]; then
+if [ ! -z "$DOMAIN" && ! -z "$EMAIL" ]; then
     snap install --classic certbot
-    certbot certonly --standalone --agree-tos --email abance@bancey.xyz --no-eff-email --non-interactive -d "$DOMAIN"
+    certbot certonly --standalone --agree-tos --email "$EMAIL" --no-eff-email --non-interactive -d "$DOMAIN"
 fi
 
 mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
 
-curl -L -o /etc/systemd/system/wings.service "https://raw.githubusercontent.com/bancey/terraform-module-pterodactyl-node/develop/provision/wings.service"
+curl -L -o /etc/systemd/system/wings.service "https://raw.githubusercontent.com/bancey/terraform-azurerm-pterodactyl-node/main/provision/wings.service"
 systemctl enable wings
