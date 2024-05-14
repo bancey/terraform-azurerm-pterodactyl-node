1abc9d90b4f34c09b1a2b4935f8ae3cff860ac1b
Alex Bance <abance@bancey.xyz>
2024-05-14 19:56:53 +0100

    feat: added identity

--- output.tf
+++ output.tf
@@ -29,6 +29,11 @@ output "vm_id" {
   value       = azurerm_linux_virtual_machine.this.id
 }
 
+output "vm_identity" {
+  description = "The identity of the VM, if enabled"
+  value       = var.enable_aad_login ? azurerm_linux_virtual_machine.this.identity : null
+}
+
 output "vm_identity_principal_id" {
   description = "The principal_id of the VM, if enabled"
   value       = var.enable_aad_login ? azurerm_linux_virtual_machine.this.identity.principal_id : null
